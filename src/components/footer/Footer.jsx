6a99e1f27674dd073603ac3d114ae6b8aa1f26ec
Muhammad Bilal Akram <bilalrathore2@gmail.com>
2023-08-14 15:48:59 +0100

    bilal

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,7 +9,7 @@ export const Footer = () => {
       <footer className='boxItems footer-color'>
         <div className='container flex'>
           <div className='social'>
-            <BsFacebook className='icon' />
+            <BsFacebook className='icon'href='https://www.wbstudiotour.co.uk/' />
             <RiInstagramFill className='icon' />
             <AiFillTwitterCircle className='icon' />
             <AiFillLinkedin className='icon' />
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,16 +7,16 @@ export const Home = () => {
   return (
     <>
       <div className='layout'>
-        <p className='typography'>WELCOME TO WHATFORD VISITS</p>;
+        <p className='typography'>WELCOME TO WATFORD VISITS</p>;
         <h1 className='explore'>EXPLORE NEW WATFORD WITH US</h1>;
       </div>
       <Category />
       <div className='aboutus'>
         <p className='aboutus-title'>About Us</p>
         <p className='aboutus-paragraph'>
-          At OurPlatform, we've created a dynamic and inclusive space where
+          At our platform, we've created a dynamic and inclusive space where
           individuals from all walks of life can come together to explore,
-          share, and engage with a world of diverse content. Whether you're an
+          share, engage with a world of diverse content and give rating according to their experience Whether you're an
           enthusiastic explorer, an aspiring creator, or simply a curious
           observer, our platform is designed to cater to your interests and
           preferences.
